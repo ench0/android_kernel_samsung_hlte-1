1afcf266a4b44e1c734c4b2912afa8e367afc43a
Wootever <Wootever@users.noreply.github.com>
2014-02-22 22:36:13 +0100

    log: quiet please

--- drivers/video/msm/mdss/mdss_samsung_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_samsung_dsi_panel.c
@@ -3518,7 +3518,7 @@ static int samsung_dsi_panel_event_handler(int event)
 		case MDSS_EVENT_RESET:
 			break;
 		default:
-			pr_err("%s : unknown event \n", __func__);
+			pr_debug("%s : unknown event \n", __func__);
 			break;
 
 	}
