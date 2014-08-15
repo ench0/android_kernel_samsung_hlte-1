9d76f3b83742c17d0605d754d86a595eabd976ce
ZION959 <ziontran@gmail.com>
2014-08-14 23:28:26 -0700

    cpufreq: interactiveX set to small "x"
    screen_off_max configurable via sysfs - default 2.27ghz (reverted from commit 4f93c9b60f526dcdc422f1a270a0af02c11a5e41)

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -148,7 +148,7 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 static
 #endif
 struct cpufreq_governor cpufreq_gov_interactive = {
-	.name = "interactiveX",
+	.name = "interactivex",
 	.governor = cpufreq_governor_interactive,
 	.max_transition_latency = 10000000,
 	.owner = THIS_MODULE,
