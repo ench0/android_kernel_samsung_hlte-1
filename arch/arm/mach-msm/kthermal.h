d47955a8d1dbde23d3deb5e460d23cd5d61f3e8c
ZION959 <ziontran@gmail.com>
2014-09-10 11:29:48 -0700

    Revert "enabled kthermal"
    This reverts commit 9bcd0ee96e9dfb8d3026073350188ea63d400d82.

--- arch/arm/mach-msm/kthermal.h
+++ arch/arm/mach-msm/kthermal.h
@@ -35,5 +35,5 @@ static struct kmsm_thermal_data kmsm_thermal_info = {
 	.freq_steps_while_throttling = 1,
 	.minimum_throttle_mhz = 918000,
 	.current_temp = 0,
-	.use_kthermal = 1,
+	.use_kthermal = 0,
 };
