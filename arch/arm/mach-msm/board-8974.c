54430009b3d12f8fa87aac427e24e2823950853f
ZION959 <ziontran@gmail.com>
2014-09-14 01:12:52 -0700

    hlte : Implement kexec-hardboot
    "Allows hard booting (i.e., with a full hardware reboot) to a kernel previously loaded in memory by kexec.  This works around the problem of soft-booted kernel hangs due
    to improper device shutdown and/or reinitialization."
    More info in /arch/arm/Kconfig.

--- arch/arm/mach-msm/board-8974.c
+++ arch/arm/mach-msm/board-8974.c
@@ -90,6 +90,13 @@ extern int msm_show_resume_irq_mask;
 #include <mach/msm8974-thermistor.h>
 #endif
 
+#ifdef CONFIG_KEXEC_HARDBOOT
+#include <asm/setup.h>
+#include <asm/memory.h>
+#include <linux/memblock.h>
+#define HLTE_PERSISTENT_RAM_SIZE (SZ_1M)
+#endif
+
 #ifdef CONFIG_ANDROID_PERSISTENT_RAM
 /* CONFIG_SEC_DEBUG reserving memory for persistent RAM*/
 #define PERSISTENT_RAM_BASE 0xbff00000
@@ -336,6 +343,16 @@ static struct i2c_board_info max77826_pmic_info[] __initdata = {
 
 void __init msm_8974_reserve(void)
 {
+#ifdef CONFIG_KEXEC_HARDBOOT
+	// Reserve space for hardboot page, just before the ram_console
+	struct membank* bank = &meminfo.bank[0];
+	phys_addr_t start = bank->start + bank->size - SZ_1M - OPPO_PERSISTENT_RAM_SIZE;
+	int ret = memblock_remove(start, SZ_1M);
+	if(!ret)
+		pr_info("Hardboot page reserved at 0x%X\n", start);
+	else
+		pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
+#endif
 	of_scan_flat_dt(dt_scan_for_memory_reserve, NULL);
 #ifdef CONFIG_ANDROID_PERSISTENT_RAM
 	persistent_ram_early_init(&per_ram);
