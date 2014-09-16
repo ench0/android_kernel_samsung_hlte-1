cd64de263710879cace04f8db432915e94363162
ZION959 <ziontran@gmail.com>
2014-09-15 18:20:57 -0700

    cpufreq: Introduce Darkness
    *But a simplified version of it compatible with the N5.  Full version was not made for this device.
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -478,6 +478,9 @@ extern struct cpufreq_governor cpufreq_gov_lazy;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_UBERDEMAND)
 extern struct cpufreq_governor cpufreq_gov_uberdemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_uberdemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
 #endif
 
 
