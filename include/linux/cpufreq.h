49fb2dd382f73b7e7d6aabce85766324a18ca707
ZION959 <ziontran@gmail.com>
2014-09-15 18:48:05 -0700

    cpu frequency : add Adaptive Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -484,6 +484,9 @@ extern struct cpufreq_governor cpufreq_gov_darkness;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_OPTIMAX)
 extern struct cpufreq_governor cpufreq_gov_optimax;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_optimax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ADAPTIVE)
+extern struct cpufreq_governor cpufreq_gov_adaptive;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_adaptive)
 #endif
 
 
